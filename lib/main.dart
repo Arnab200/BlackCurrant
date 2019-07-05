9b094632876f22f117a43e1244d3651202905c34
Arnab Mukhopadhyay <arnab.mukhopadhyay@hitachiconsulting.com>
2019-07-05 16:44:03 +0530

    Updating real-time GPS location

--- lib/main.dart
+++ lib/main.dart
@@ -3,45 +3,55 @@ import 'package:location/location.dart';
 
 void main() => runApp(LocationManager());
 
-class LocationManager extends StatefulWidget{
+class LocationManager extends StatefulWidget {
   @override
   _LocationManagerState createState() => _LocationManagerState();
 }
 
-
-class _LocationManagerState extends State<LocationManager>{
-
+class _LocationManagerState extends State<LocationManager> {
   Location location = new Location();
+  double Latitude = 0;
+  double Longitude = 0;
 
   Map<String, double> currentLocation;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    location.onLocationChanged().listen((value){
-      setState((){
+    location.onLocationChanged().listen((value) {
+      setState(() {
         currentLocation = value;
-});
+      });
     });
-}
-
+  }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LocationManager',
+        title: 'LocationManager',
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('LocationManager'),
-      ),
-      body: Center(
-
-        child: Column(
-          children: <Widget>[currentLocation == null ?
-          CircularProgressIndicator() : Text('Location:' + currentLocation["latitude"].toString() + " " + currentLocation["longitude"].toString()) ],
-        ),
-      )
-    )
-    );
+            appBar: AppBar(
+              title: Text('LocationManager'),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[currentLocation == null ? CircularProgressIndicator():
+                  Text("Lat: ${Latitude}" + " " + "Long: ${Longitude}" ),
+                  RaisedButton(
+                    color: Colors.blue,
+                    onPressed: getLocation,
+                    child: const Text('Get Location',
+                        style: TextStyle(fontWeight: FontWeight.normal)),
+                  ),
+                ],
+              ),
+            )));
   }
+
+  void getLocation(){
+    Latitude = currentLocation['latitude'];
+    Longitude = currentLocation['longitude'];
+  }
+
 }
